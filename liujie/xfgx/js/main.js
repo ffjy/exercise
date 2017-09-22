25aa6e329b936144b2d87417180d3ecc41487ceb
WoodenMan001 <347335621@qq.com>
2017-09-22 18:33:34 +0800

    liujie:小凡公学

---
+++ liujie/xfgx/js/main.js
@@ -0,0 +1,33 @@
+var new_scroll_position = 0;
+var last_scroll_position;
+var header = document.getElementById("header");
+
+window.addEventListener('scroll', function(e) {
+    last_scroll_position = window.scrollY;
+
+    var height = (document.URL=="http://localhost:8000/aboutUs.html") ? 389 : 766;
+
+    // 向下滚动
+    if ( last_scroll_position > height) {
+        header.classList.remove("slideDown");
+        header.classList.add("slideUp");
+
+        // 向上滚动
+    } else if (height > last_scroll_position) {
+        header.classList.remove("slideUp");
+        header.classList.add("slideDown");
+    }
+
+    new_scroll_position = last_scroll_position;
+});
+
+var posterTvGrid = new posterTvGrid(
+    'posterTvGrid',{className: "posterTvGrid"},
+    [
+
+        {"img":"img\/main\/icon_01.png","title":"Lorem ipsum dolor sit amet","url":"#"},
+        {"img":"img\/main\/icon_03.png","title":"Lorem ipsum dolor sit amet","url":"#"},
+        {"img":"img\/main\/icon_04.png","title":"Lorem ipsum dolor sit amet","url":"#"},
+        {"img":"img\/main\/icon_img2.png","title":"Lorem ipsum dolor sit amet","url":"#"}
+    ]
+);
