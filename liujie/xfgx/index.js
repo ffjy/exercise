631ef4553f0f7937ae80f27f8412411a4d0750d2
maczyt <369280416@qq.com>
2017-09-27 19:24:44 +0800

    ok

--- liujie/xfgx/index.js
+++ liujie/xfgx/index.js
@@ -1,11 +1,27 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 var join = require('path').join;
+var mail = require('./mail');
 
 app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.get('/', function(req, res){
 	res.status(200).sendFile(join(__dirname, 'index.html'));
 });
 
-app.listen(3000);
+app.post('/leaveWord', function(req, res) {
+	var username = req.body.user_name,
+		mobile = req.body.user_telephone,
+		leaveWord = req.body.user_leaveWord;
+	mail(username, mobile, leaveWord, function(err, msg) {
+		if (err) { console.log(err.message);}
+		// return res.send(msg);
+	})
+	res.send('留言成功，谢谢🙏');
+});
+
+app.listen(8888, function() { console.log('server start') });
 
