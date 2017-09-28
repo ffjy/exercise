21478b1bd420e99b968cdf3377aecbcb24ebdd8c
WoodenMan001 <347335621@qq.com>
2017-09-28 09:56:58 +0800

    liujie:修改‘发邮件’代码

--- liujie/xfgx/index.js
+++ liujie/xfgx/index.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 var app = express();
 var join = require('path').join;
 var mail = require('./mail');
 
 app.use(express.static(__dirname));
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
